30aecd5afab5f814297cfad9d2a875406dfced07
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2024-11-09 10:16:38 +0530

    code

---
+++ Nov_9/QUestions.sql
@@ -0,0 +1,30 @@
+-- Display total no.of records from Customer table
+
+-- Display total records by menu_type example : Non-Veg 4 Veg 3 Beragage 2 
+
+-- Display all records from Menu Table
+
+-- Display authentication query for vendor table as enter username and passcode if correct return 1 else return 0
+
+-- Apply All Joins on Customer and Wallet tables
+
+-- Display customer details who have wallets
+
+-- display Customer Details who don't have wallets
+
+-- Apply Joins between customer, menu, orders, vendor tables 
+
+-- Display customer details who placed orders
+
+-- Display customer details who don't placed any orders
+
+-- Display vendor details who recieved orders
+
+-- Display Vendors who got more than one order
+
+-- Display all customer details who got more than one wallet
+
+-- Display Menu Item details which is got more than one order 
+
+-- Display all customer details who have wallet 'PAYTM'
+
