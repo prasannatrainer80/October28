7e66908e40123f2d4732bbb7b346ba4cbc050456
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2024-11-16 13:57:59 +0530

    code

---
+++ Nov_16/JdbcDemo/src/main/java/com/java/jdbc/EmployDelete.java
@@ -0,0 +1,33 @@
+package com.java.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class EmployDelete {
+
+	public static void main(String[] args) {
+		int empno;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Employ No   ");
+		empno = sc.nextInt();
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hexapracticeoctober",
+					"root", "root");
+			String cmd = "Delete from EmployNew where Empno = ?";
+			PreparedStatement pst = connection.prepareStatement(cmd);
+			pst.setInt(1, empno);
+			pst.executeUpdate();
+			System.out.println("*** Employ Record Deleted ***");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
